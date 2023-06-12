dec2c9754a48331c1c3e76d9671695d4f754a135
Deepanshu Rathore <133076945+Deepanshu-975@users.noreply.github.com>
2023-06-12 16:04:05 +0530

    Add files via upload
    Maximum subarraysum

---
+++ MaximumSubarraysum/Maxisubarray.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> 
+long long maxSubarraySum(int arr[], int n)
+{
+    long long maxi = 0; // maximum sum
+    long long sum = 0;
+
+    int start = 0;
+    int ansStart = -1, ansEnd = -1;
+    for (int i = 0; i < n; i++) {
+
+        if (sum == 0) start = i; // starting index
+
+        sum += arr[i];
+
+        if (sum > maxi) {
+            maxi = sum;
+
+            ansStart = start;
+            ansEnd = i;
+        }
+
+        // If sum < 0: discard the sum calculated
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+    return maxi;
+}
