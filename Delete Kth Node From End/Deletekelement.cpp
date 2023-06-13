96ac953b07813893b58add574efd93679351e261
Deepanshu Rathore <133076945+Deepanshu-975@users.noreply.github.com>
2023-06-13 10:33:08 +0530

    Add files via upload
    Delete a node form K element

---
+++ Delete Kth Node From End/Deletekelement.cpp
@@ -0,0 +1,45 @@
+/*
+Following is the class structure of the Node class:
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = NULL;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+*/
+
+Node* removeKthNode(Node* head, int K)
+{
+     Node * start = new Node();
+        start -> next = head;
+        Node* fast = start;
+        Node* slow = start;     
+
+        for(int i = 1; i <= K; ++i)
+            fast = fast->next;
+
+        while (fast->next != NULL) {
+          fast = fast->next;
+          slow = slow->next;
+        }
+
+        slow->next = slow->next->next;
+
+        return start->next;
+}
