045269f33a2601ff6f3669158408f1eb76468abc
Deepanshu Rathore <133076945+Deepanshu-975@users.noreply.github.com>
2023-06-28 00:05:10 +0530

    Add files via upload

---
+++ Add two numbers/twonumadd.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition of linked list:
+ *
+ * class Node {
+ * public:
+ *      int data;
+ *      Node *next;
+ *      Node() {
+ *          this->data = 0;
+ *          this->next = NULL;
+ *      }
+ *      Node(int data) {
+ *          this->data = data;
+ *          this->next = NULL;
+ *      }
+ *      Node (int data, Node *next) {
+ *          this->data = data;
+ *          this->next = next;
+ *      }
+ * };
+ *
+ *************************************************************************/
+
+Node *addTwoNumbers(Node *num1, Node *num2)
+{
+    Node* dummy =new Node();
+    Node* temp =dummy;
+    int carry =0;
+    while(num1 !=NULL || num2!= NULL || carry){
+        int sum =0;
+        if(num1 !=NULL){
+            sum +=num1->data;
+            num1 =num1->next;
+        }
+        if(num2 !=NULL){
+            sum +=num2->data;
+            num2 =num2->next;
+        }
+
+        sum +=carry;
+        carry =sum/10;
+        Node* newNode =new Node(sum%10);
+        temp->next=newNode;
+        temp =temp->next;
+    }
+    return dummy->next;
+}
